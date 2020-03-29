c76012577b939a089417198648a2fa1b270b1bc7
Fenil Shah <36612912+fcshah@users.noreply.github.com>
2020-03-29 01:53:16 -0300

    keep trying bro

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,7 @@ export default () => {
     			<i className="fa fa-twitter"></i>
     		</a>
         </button> */}
-    		{/* <button className="icon-btn facebook">
+    		<button className="icon-btn facebook">
     		<a className="link" href="https://www.facebook.com/fenil.shah.5070" target="_blank">
     			<i className="fa fa-facebook" ></i>
     		</a>
@@ -26,26 +26,10 @@ export default () => {
     		</a>
         </button>
     		<button className="icon-btn github">
-    		<a className="link" href="https://github.com/fcshah" target="_blank">
+    		<a className="link" href="https://github.com/fcshah" style={{float:"center"}} target="_blank">
     			<i className="fa fa-github"></i>
     		</a>
-        </button> */}
-        {/* ------------------------------------------------------------------------------------------------------------------ */}
-        	{/* <button class="icon-btn twitter"> 
-			<i class="fa fa-twitter"></i>
-		</button> */}
-		<button class="icon-btn facebook"> 
-			<i class="fa fa-facebook"></i>
-		</button>
-		<button class="icon-btn google-plus"> 
-			<i class="fa fa-google-plus"></i>
-		</button>
-		<button class="icon-btn instagram"> 
-			<i class="fa fa-instagram"></i>
-		</button>
-		<button class="icon-btn github"> 
-			<i class="fa fa-github"></i>
-		</button>
+        </button>
     </div>
 
     // </div>
