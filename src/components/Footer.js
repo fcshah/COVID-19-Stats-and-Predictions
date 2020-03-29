768facbbf2529f554b18bda85873289a4e5d7df7
Fenil Shah <36612912+fcshah@users.noreply.github.com>
2020-03-29 00:43:23 -0300

    one more try

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,14 +2,14 @@ import React from "react"
 
 export default () => {
   return (
-    <div className="footer">
+    <div className="footer social">
 {/* <div className="social"> */}
     		<h3><i> Check me out below! </i></h3>
-    		{/* <button className="icon-btn twitter">
+    		<button className="icon-btn twitter">
     		<a className="link" href="https://twitter.com/" target="_blank">
     			<i className="fa fa-twitter"></i>
     		</a>
-        </button> */}
+        </button>
     		<button className="icon-btn facebook">
     		<a className="link" href="https://www.facebook.com/fenil.shah.5070" target="_blank">
     			<i className="fa fa-facebook" ></i>
@@ -17,12 +17,12 @@ export default () => {
         </button>
     		<button className="icon-btn twitter">
     		<a className="link" href="https://www.linkedin.com/in/sfenil/" target="_blank">
-        <i class="fa fa-linkedin-square" style={{  color:"#3a3ad7" }}></i>
+        <i class="fa fa-linkedin-square" ></i>
     		</a>
         </button>
     		<button className="icon-btn instagram">
     		<a className="link" href="https://www.instagram.com/_fenilshah_/"  target="_blank">
-    			<i className="fa fa-instagram" style={{alignSelf:"center"}}></i>
+    			<i className="fa fa-instagram"></i>
     		</a>
         </button>
     		<button className="icon-btn github">
